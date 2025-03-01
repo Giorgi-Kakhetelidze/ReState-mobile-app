4f82aa77ac07e5ebb0e99815eadbff05fc462e0a
Kaxeta007 <giorgikakhetelidze73@gmail.com>
2025-03-01 21:31:42 +0400

    first commit

---
+++ lib/global-provider.tsx
@@ -0,0 +1,57 @@
+import React, { createContext, useContext, ReactNode } from "react";
+
+import { getCurrentUser } from "./appwrite";
+import { useAppwrite } from "./useAppwrite";
+
+export interface User {
+  $id: string;
+  name: string;
+  email: string;
+  avatar: string;
+}
+
+interface GlobalContext {
+  isLoggedIn: boolean;
+  user: User | null;
+  loading: boolean;
+  refetch: (newParams?: Record<string, string | number>) => Promise<void>;
+}
+
+const GlobalContext = createContext<GlobalContext | undefined>(undefined);
+
+export const GlobalProvider = ({ children }: { children: ReactNode }) => {
+  const {
+    data: user,
+    loading,
+    refetch,
+  } = useAppwrite({
+    fn: getCurrentUser,
+  });
+
+  const isLoggedIn = !!user;
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        isLoggedIn,
+        user,
+        loading,
+        refetch,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export const useGlobalContext = (): GlobalContext => {
+  const context = useContext(GlobalContext);
+
+  if (!context) {
+    throw new Error("useGlobalContext must be used within a GlobalProvider");
+  }
+
+  return context;
+};
+
+export default GlobalProvider;
